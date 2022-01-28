4d95b085098184c6a267570ad4a2426e45bdb8d1
Billy Mott <billygm@uw.edu>
2022-01-28 11:20:15 -0800

    adding/fixing GET route

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
-const express = require('express');
-const path = require('path');
-const noteData = require('./db/db.json');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const uuid = require("uuid");
 const PORT = 3001;
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/notes.html'));
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-app.get('/api/notes', (req,res) => {
-    res.json(noteData)
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", (err, data) => {
+    const notes = JSON.parse(data);
+    if (err) {
+      console.error(err);
+    } else {
+      res.json(notes);
+    }
+  });
 });
-
-
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
